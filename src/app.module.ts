a71185f3bf98f679ef3795e0d31ffb156a3af2bf
9029David <davidalejandroalvarez@gmail.com>
2025-12-27 08:09:24 -0300

    feat implementacion de base de datos postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,14 +11,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
         autoLoadEntities: true,
-        synchronize: true,
+        synchronize: false,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       }),
     }),
     TasksModule,
